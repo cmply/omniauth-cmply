1b9feb1eea979e849f1e65d9996fbbfc36c5f5cf
Michael De Lorenzo <michael@delorenzodesign.com>
2012-09-13 10:02:15 -0400

    Enable changing API site based on RACK_ENV
    Michael De Lorenzo
    michael@delorenzodesign.com

--- lib/omniauth/strategies/cmply.rb
+++ lib/omniauth/strategies/cmply.rb
@@ -5,7 +5,10 @@ module OmniAuth
   module Strategies
     class Cmply < OmniAuth::Strategies::OAuth2
       option :name, 'cmply'
-      option :client_options, {:authorize_path => '/oauth/authorize',:site => 'https://api.cmp.ly'}
+      option :client_options, {
+                                  :authorize_path => '/oauth/authorize',
+                                  :site => ENV["RACK_ENV"].eql?("development") ? 'http://api.cmply.local:8800' : 'http://api.cmp.ly'
+      }
 
       #uid { access_token.params[:user_id] }
       uid { raw_info['_id'] }
